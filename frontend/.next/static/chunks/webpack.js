b37b43a31f2b9fbb76fd5b19e4282c6ae6caaccd
utkarshbhai007 <dr.utkarshbarad@gmail.com>
2025-09-30 10:38:41 +0530

    first commit

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b874bbde912394c"; }
+/******/ 		__webpack_require__.h = function() { return "d7d6ade9549a60ac"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
